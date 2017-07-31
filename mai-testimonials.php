fdce038e21a3ee5439208d30fdb5229142dd920b
Mike Hemberger <mike@thestizmedia.com>
2017-07-31 17:02:08 -0400

    Updater and [grid] integration

--- mai-testimonials.php
+++ mai-testimonials.php
@@ -144,6 +144,8 @@ final class Mai_Testimonials_Setup {
 		}
 		// Includes
 		$this->includes();
+		// Run
+		$this->run();
 	}
 
 	function deactivate_plugin() {
@@ -166,29 +168,49 @@ final class Mai_Testimonials_Setup {
 	 */
 	private function includes() {
 		foreach ( glob( MAI_TESTIMONIALS_INCLUDES_DIR . '*.php' ) as $file ) { include $file; }
-		require_once( MAI_TESTIMONIALS_INCLUDES_DIR . 'vendor/extended-cpts.php' );
-		require_once( MAI_TESTIMONIALS_INCLUDES_DIR . 'vendor/extended-taxos.php' );
+		require_once MAI_TESTIMONIALS_INCLUDES_DIR . 'vendor/extended-cpts.php';
+		require_once MAI_TESTIMONIALS_INCLUDES_DIR . 'vendor/extended-taxos.php';
+		require_once MAI_TESTIMONIALS_INCLUDES_DIR . 'vendor/plugin-update-checker/plugin-update-checker.php';
 	}
 
-	public function setup() {
-
-		add_action( 'init',               array( $this, 'register_content_types' ) );
-		add_action( 'template_redirect',  array( $this, 'redirect' ) );
+	public function run() {
 
 		register_activation_hook(   __FILE__, array( $this, 'activate' ) );
 		register_deactivation_hook( __FILE__, 'flush_rewrite_rules' );
+
+		add_action( 'init',                array( $this, 'register_content_types' ) );
+		add_action( 'template_redirect',   array( $this, 'redirect' ) );
+		add_action( 'cmb2_admin_init',     array( $this, 'metabox' ) );
+		add_action( 'wp_enqueue_scripts',  array( $this, 'enqueue_scripts' ) );
+
+		add_filter( 'shortcode_atts_grid',         array( $this, 'grid_atts' ), 8, 3 );
+		add_filter( 'genesis_attr_flex-entry',     array( $this, 'flex_entry_atts'), 12, 3 );
+		add_filter( 'genesis_attr_entry-content',  array( $this, 'entry_content_atts'), 12, 3 );
+		add_filter( 'genesis_attr_entry-header',   array( $this, 'entry_header_atts'), 12, 3 );
+		add_filter( 'genesis_attr_entry-title',    array( $this, 'entry_title_atts'), 12, 3 );
+		add_filter( 'mai_flex_entry_header',       array( $this, 'add_author_details' ), 10, 2 );
+
+		// Setup the updater
+		$updater = Puc_v4_Factory::buildUpdateChecker( 'https://github.com/bizbudding/mai-testimonials/', __FILE__, 'mai-testimonials' );
+	}
+
+	public function activate() {
+		$this->register_content_types();
+		flush_rewrite_rules();
 	}
 
 	public function register_content_types() {
 
 		$args = array(
+			'enter_title_here'    => __( 'Enter customer name here', 'mai-testimonials' ),
+			'exclude_from_search' => true,
+			'featured_image'      => __( 'Testimonial Image', 'mai-testimonials' ),
 			'menu_icon'           => 'dashicons-format-quote',
 			'public'              => false,
 			'publicly_queryable'  => true,
-			'show_ui'             => true,
 			'show_in_menu'        => true,
 			'show_in_nav_menus'   => false,
-			'exclude_from_search' => true,
+			'show_ui'             => true,
 			'supports'            => array( 'title', 'editor', 'thumbnail' ),
 		);
 
@@ -202,11 +224,6 @@ final class Mai_Testimonials_Setup {
 		register_extended_post_type( 'testimonial', apply_filters( 'mai_testimonial_args', $args ), apply_filters( 'mai_testimonial_labels', $labels ) );
 	}
 
-	public function activate() {
-		$this->register_content_types();
-		flush_rewrite_rules();
-	}
-
 	// Redirect if trying to view a single testimonial
 	public function redirect() {
 		if ( ! is_singular( 'testimonial' ) ) {
@@ -214,6 +231,159 @@ final class Mai_Testimonials_Setup {
 		}
 		wp_redirect( home_url() );
 		exit();
+	}
+
+	/**
+	 * Define the metabox and field configurations.
+	 */
+	function metabox() {
+
+		// Initiate the metabox
+		$cmb = new_cmb2_box( array(
+			'id'           => 'mai_testimonials',
+			'title'        => __( 'Testimonial Details', 'mai-testimonials' ),
+			'object_types' => array( 'testimonial' ),
+			'context'      => 'normal',
+			'priority'     => 'high',
+			'show_names'   => true, // Show field names on the left
+		) );
+
+		// Regular text field
+		$cmb->add_field( array(
+			'name' => __( 'Byline', 'mai-testimonials' ),
+			'desc' => __( 'Enter a byline for the customer giving this testimonial (for example: "CEO of MaiPro").', 'mai-testimonials' ),
+			'id'   => 'byline',
+			'type' => 'text',
+		) );
+
+		// URL text field
+		$cmb->add_field( array(
+			'name' => __( 'Website URL', 'mai-testimonials' ),
+			'desc' => __( 'Enter a URL that applies to this customer (for example: https://maipro.io).', 'mai-testimonials' ),
+			'id'   => 'url',
+			'type' => 'text_url',
+		) );
+	}
+
+	function enqueue_scripts() {
+		// Register CSS file for later
+		wp_register_style( 'mai-testimonials', MAI_TESTIMONIALS_PLUGIN_URL . 'assets/mai-testimonials.css', array(), MAI_TESTIMONIALS_VERSION );
+	}
+
+	/**
+	 * Filter the default args for [grid] shortcode when displaying testimonials.
+	 *
+	 * @param   array  $out    The modified attributes.
+	 * @param   array  $pairs  Entire list of supported attributes and their defaults.
+	 * @param   array  $atts   User defined attributes in shortcode tag.
+	 *
+	 * @return  array  The modified attributes.
+	 */
+	function grid_atts( $out, $pairs, $atts ) {
+
+		// Bail if not a testimonial
+		if ( 'testimonial' !== $atts['content'] ) {
+			return $out;
+		}
+
+		// Enqueue CSS file
+		wp_enqueue_style( 'mai-testimonials' );
+
+		if ( ! isset( $atts['align'] ) ) {
+			$out['align'] = 'center, middle';
+		}
+
+		if ( ! isset( $atts['columns'] ) ) {
+			$out['columns'] = 2;
+		}
+
+		if ( ! isset( $atts['image_size'] ) ) {
+			$out['image_size'] = 'thumbnail';
+		}
+
+		if ( ! isset( $atts['link'] ) ) {
+			$out['link'] = false;
+		}
+
+		if ( ! isset( $atts['show'] ) ) {
+			$out['show'] = 'image, title, content';
+		}
+
+		if ( ! isset( $atts['title_wrap'] ) ) {
+			$out['title_wrap'] = 'span';
+		}
+
+		return $out;
+	}
+
+	function flex_entry_atts( $attributes, $context, $atts ) {
+		// Bail if not a testimonial.
+		if ( ! $this->is_testimonial( $atts ) ) {
+			return $attributes;
+		}
+		$attributes['itemprop'] = 'review';
+		$attributes['itemtype'] = 'http://schema.org/Review';
+		return $attributes;
+	}
+
+	function entry_content_atts( $attributes, $context, $atts ) {
+		// Bail if not a testimonial.
+		if ( ! $this->is_testimonial( $atts ) ) {
+			return $attributes;
+		}
+		$attributes['itemprop'] = 'reviewBody';
+		return $attributes;
+	}
+
+	function entry_header_atts( $attributes, $context, $atts ) {
+		// Bail if not a testimonial.
+		if ( ! $this->is_testimonial( $atts ) ) {
+			return $attributes;
+		}
+		$attributes['itemprop'] = 'author';
+		$attributes['itemtype'] = 'http://schema.org/Person';
+		return $attributes;
+	}
+
+	function entry_title_atts( $attributes, $context, $atts ) {
+		// Bail if not a testimonial.
+		if ( ! $this->is_testimonial( $atts ) ) {
+			return $attributes;
+		}
+		$attributes['itemprop'] = 'name';
+		return $attributes;
+	}
+
+	function add_author_details( $entry_header, $atts ) {
+		// Bail if not a testimonial.
+		if ( ! $this->is_testimonial( $atts ) ) {
+			return $entry_header;
+		}
+		// Byline
+		$byline = get_post_meta( get_the_ID(), 'byline', true );
+		if ( $byline ) {
+			$entry_header .= sprintf( '<span class="title" itemprop="jobTitle">%s</span>', sanitize_text_field( $byline ) );
+		}
+		// URL
+		$url = get_post_meta( get_the_ID(), 'url', true );
+		if ( $url ) {
+			$url = esc_url( $url );
+			$entry_header .= sprintf( '<span class="url"><a href="%s" itemprop="url">%s</a></span>', $url, $url );
+		}
+		return $entry_header;
+	}
+
+	function is_testimonial( $atts ) {
+		// Bail if we have no atts.
+		if ( ! isset( $atts ) || ! is_array( $atts ) ) {
+			return false;
+		}
+		// Bail if not a testimonial.
+		if ( ! isset( $atts['content'] ) || ! in_array( 'testimonial', (array) $atts['content'] ) ) {
+			return false;
+		}
+		// Yay, a testimonial.
+		return true;
 	}
 
 }
