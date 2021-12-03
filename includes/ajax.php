ae332ca51fc22259b293c3063cea65ec05435dd0
Mike Hemberger <mike@thestizmedia.com>
2021-12-03 13:27:32 -0500

    cleanup

--- includes/ajax.php
+++ includes/ajax.php
@@ -3,21 +3,28 @@
 // Prevent direct file access.
 defined( 'ABSPATH' ) || die;
 
-add_action( 'wp_ajax_mait_load_more_posts', 'mait_load_more_posts' );
-add_action( 'wp_ajax_nopriv_mait_load_more_posts', 'mait_load_more_posts' );
-function mait_load_more_posts() {
+add_action( 'wp_ajax_mait_ajax_get_testimonials', 'mait_ajax_get_testimonials' );
+add_action( 'wp_ajax_nopriv_mait_ajax_get_testimonials', 'mait_ajax_get_testimonials' );
+/**
+ * Gets testimonials via ajax.
+ *
+ * @access private
+ *
+ * @since TBD
+ *
+ * @return void
+ */
+function mait_ajax_get_testimonials() {
 	$security = check_ajax_referer( 'mai_testimonials_slider', 'nonce' );
 
 	if ( false === $security ) {
-		// return;
-		// wp_die();
-		return wp_send_json_error();
+		wp_send_json_error();
+		wp_die();
 	}
 
 	if ( ! isset( $_POST['block_args'] ) ) {
-		// return;
-		// wp_die();
-		return wp_send_json_error();
+		wp_send_json_error();
+		wp_die();
 	}
 
 	$args         = wp_unslash( $_POST['block_args'] );
@@ -31,23 +38,9 @@ function mait_load_more_posts() {
 		'next'  => $testimonials->next,
 	];
 
-	// ray( $_POST );
-	// $query_args = json_decode( $_POST['query_args'] );
-
-	// ray( $query_args );
-	// ray( $_POST['posts_per_page'] );
-	// $result = [
-	// 	'success' => true,
-	// 	'message' => 'Sweeeeet!',
-	// ];
-
-	// $result = '<div class="mai-testimonials">';
-	// 	$result .= '<h2>This is working?!?!?</h2>';
-	// $result .= '</div>';
-
-	// Make your array as json
+	// Make your array as json.
 	wp_send_json_success( $data );
 
-	// Don't forget to stop execution afterward.
+	// Die.
 	wp_die();
 }
